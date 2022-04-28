e48ea2e2578374706db9647db2fa6ef3c86616eb
Alena Abrazhenina <akino93@ya.ru>
2022-04-28 17:40:22 +0400

    publish text to Telegram channel

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
 
 import datetime
+import logging
 import requests
+import telegram
 
 from dotenv import load_dotenv
 from pathlib import Path
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 url_img1 = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 url_spacex = 'https://api.spacexdata.com/v4/launches/'
@@ -86,19 +98,68 @@ def fetch_nasa_epic_imgs(url_data, url_img, api_key, dir):
             img.write(response.content)
 
 
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+
+
 if __name__ == '__main__':
     load_dotenv()
     API_KEY = os.environ['API_KEY']
     img_name = 'hubble.jpg'
     dir = f'images'
     Path(dir).mkdir(parents=True, exist_ok=True)
-    download_img(url_img1, dir, img_name)
+    #download_img(url_img1, dir, img_name)
     dir = f'images'
     Path(dir).mkdir(parents=True, exist_ok=True)
-    fetch_spacex_imgs(url_spacex, dir)
+    #fetch_spacex_imgs(url_spacex, dir)
     dir = f'images/nasa'
     Path(dir).mkdir(parents=True, exist_ok=True)
-    fetch_nasa_imgs(url_nasa, API_KEY, dir)
+    #fetch_nasa_imgs(url_nasa, API_KEY, dir)
     dir = f'images/nasa/epic'
     Path(dir).mkdir(parents=True, exist_ok=True)
-    fetch_nasa_epic_imgs(url_nasa_epic_data, url_nasa_epic_img, API_KEY, dir)
+    #fetch_nasa_epic_imgs(url_nasa_epic_data, url_nasa_epic_img, API_KEY, dir)
+
+    tg_token = os.environ['TG_TOKEN']
+    bot = telegram.Bot(token=tg_token)
+    print(bot.get_me())
+    chat_id = 'https://t.me/nasa_cosmos_photos'
+    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+
+    updater = Updater(tg_token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dispatcher.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
